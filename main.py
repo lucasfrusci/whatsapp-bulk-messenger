d7a3b38f26163012c007b675595a7de5802865bd
lucasfrusci <lucasfrusci@gmail.com>
2025-02-07 16:57:50 -0300

    atualizando visual

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ janela.resizable(False,False)
 labelTop = ctk.CTkLabel(janela, text="WhatsApp MENSAGEM AUTO", font=("Segoe UI", 20), height=10, width=10, text_color="green").place(x=10, y=10)
 
 #TEXTBOX NUMEROS
-tbNumeros = ctk.CTkTextbox(janela, width=250, height=200)
+tbNumeros = ctk.CTkTextbox(janela, width=180, height=200)
 tbNumeros.insert("0.0", "ADICIONE AQUI OS CONTATOS")
 tbNumeros.place(x=10, y=50)
 
 #TEXTBOX MENSAGEM
-tbMensagem = ctk.CTkTextbox(janela, width=250, height=200)
+tbMensagem = ctk.CTkTextbox(janela, width=590, height=200)
 tbMensagem.insert("0.0", "Digite a mensagem")
-tbMensagem.place(x=280, y=50)
+tbMensagem.place(x=200, y=50)
 
 #TEXTBOX LOG
 tbLog = ctk.CTkTextbox(janela, width=780, height=100)
